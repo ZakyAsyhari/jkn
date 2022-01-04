08ffff089e8a1fe8aeceeecec2c0e17b3713c660
Tmbng <zakky@mbi.biz.id>
2022-01-04 21:47:54 +0700

    db

--- elisabet_apps.sql
+++ elisabet_apps.sql
@@ -3,13 +3,11 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Dec 31, 2021 at 06:47 PM
+-- Generation Time: Jan 04, 2022 at 09:46 PM
 -- Server version: 8.0.27-0ubuntu0.20.04.1
 -- PHP Version: 7.2.34-28+ubuntu20.04.1+deb.sury.org+1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -116,7 +114,6 @@ ALTER TABLE `ok_jadwal_operasi`
 --
 ALTER TABLE `ok_jadwal_operasi`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
--- elisabet_core.sql
+++ elisabet_core.sql
@@ -3,13 +3,11 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Dec 31, 2021 at 06:47 PM
+-- Generation Time: Jan 04, 2022 at 09:47 PM
 -- Server version: 8.0.27-0ubuntu0.20.04.1
 -- PHP Version: 7.2.34-28+ubuntu20.04.1+deb.sury.org+1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -2233,7 +2231,6 @@ ALTER TABLE `mpoli`
 --
 ALTER TABLE `mr_jadwal_tetap`
   MODIFY `Id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3810;
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
