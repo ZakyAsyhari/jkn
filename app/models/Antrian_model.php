9088f8fadde68762046df506ae7c9809fc96d26f
Tmbng <zakky@mbi.biz.id>
2022-05-31 08:43:00 +0700

    update query

--- app/models/Antrian_model.php
+++ app/models/Antrian_model.php
@@ -303,8 +303,8 @@ class Antrian_model extends CI_Model
     }
 
     public function get_non_jkn(){
-        $cek = $this->db->query("SELECT * from mr_periksa order by id desc limit 20")->result_array();
-        debug($cek);
+        // $cek = $this->db->query("SELECT * from mr_periksa order by id desc limit 20")->result_array();
+        // debug($cek);
         $dnonjkn = $this->db->query("SELECT mrp.rm as norm,mrk.ktp as nik ,mmr.hp,mrp.tgldaftar as tanggalperiksa,
 					muser.nm_user as nm_dokter,muser.id_extpass as kode_dokter,
 					mpoli.s_name as kodepoli,mpoli.poli,muser.nik as iddokter,mpoli.nama as nama_poli
@@ -313,10 +313,7 @@ class Antrian_model extends CI_Model
 					join mr_ktp mrk on mrk.rm = mmr.rm
 					join muser on muser.nik = mrp.kode_dok
 					join mpoli on mpoli.poli = mrp.poli
-					-- join mr_jadwal_tetap mrj on mrj.dokter = muser.nik and mrj.poli = mpoli.poli
-                    WHERE muser.id_extpass is not null and mpoli.s_name is not null
-					-- AND NOT EXISTS (select norm,nik,kodepoli,iddokter from antrian_jkn)
-					-- group by mrp.rm,mrk.ktp,mmr.hp,mrp.tgldaftar,muser.nm_user,muser.id_extpass,mpoli.s_name,muser.nik,mpoli.poli,mpoli.nama 
+					
                     order by mrp.id desc 
                     LIMIT 10")->result_array();
                     debug($dnonjkn);
