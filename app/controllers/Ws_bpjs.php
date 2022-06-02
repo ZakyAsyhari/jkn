b5502302cb20f6317c30b3b5054fd832c65dc038
Tmbng <zakky@mbi.biz.id>
2022-06-02 13:32:58 +0700

    test

--- app/controllers/Ws_bpjs.php
+++ app/controllers/Ws_bpjs.php
@@ -263,9 +263,8 @@ var $basehfis		= 'https://apijkn.bpjs-kesehatan.go.id/antreanrs/';
 		// cek data di mr_karcis cetak
 		$datas = $this->db->query("SELECT ap.*,muser.id_extPass as kode_dokter
 								   from antrian_jkn ap
-								   join mpoli on mpoli.s_name = ap.kodepoli
 								   join muser on muser.nik = ap.iddokter
-								   join (select rm,dokter,poli,count(*) from mr_karcis_cetak group by rm,dokter,poli) mrk on mrk.rm = ap.norm
+								--    join (select rm,dokter,poli,count(*) from mr_karcis_cetak group by rm,dokter,poli) mrk on mrk.rm = ap.norm
 								   where ap.flag_ws = 'N' and date(ap.tglinsert) = '$tglsekarang' 
 								   LIMIT 10
 								")->result_array();
