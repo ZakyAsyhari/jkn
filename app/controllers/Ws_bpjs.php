363675e37186e0e3f0e543d1dfcce6c805124895
Tmbng <zakky@mbi.biz.id>
2022-09-14 14:57:15 +0700

    cek data

--- app/controllers/Ws_bpjs.php
+++ app/controllers/Ws_bpjs.php
@@ -265,7 +265,7 @@ var $basehfis		= 'https://apijkn.bpjs-kesehatan.go.id/antreanrs/';
 		$datas = $this->db->query("SELECT ap.*,muser.id_extPass as kode_dokter
 								   from antrian_jkn ap
 								   join muser on muser.nik = ap.iddokter
-								   where (ap.flag_ws = 'N' or ap.flag_ws is null) and ap.tanggalperiksa = '$tglsekarang' order by ap.id desc
+								   where (ap.flag_ws = 'N' or ap.flag_ws is null) and ap.tanggalperiksa = '$tglsekarang' order by ap.id asc
 								   LIMIT 25
 								")->result_array();
 
